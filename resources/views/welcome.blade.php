07b0c42f9598852df689ea90d590fac3fcb9c243
seumsims <seumworldinbangladesh@gmail.com>
2022-06-10 19:50:49 +0600

    master laravel 8 and react issue during installation solution

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        <div id="example">
+
+        </div>
+
+    <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
